431e33b60cb441c26474b69b7c2ddbb9b39eb6f2
Stahayd <you@example.com>
2023-02-18 11:04:57 -0800

    Hello, printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main -  Entry Point
+ * main - this is the main function
  *
- * Return: Always 0 (Success) 
+ * Return: always 0
  */
 
-int main (void) 
+int main(void) 
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("/"Programming is like building a multilingual puzzle);
 	return (0);
-
 }
